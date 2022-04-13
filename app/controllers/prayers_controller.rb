ba3ee9f5427f8cee25192e9c888680ae4440127f
FerrariJavier <javierferrarif@gmail.com>
2022-04-13 10:42:10 +0100

    DB migration

--- app/controllers/prayers_controller.rb
+++ app/controllers/prayers_controller.rb
@@ -1,5 +1,8 @@
 class PrayersController < ApplicationController
+
   def index
     render json: { foo: 'bar' }
+    # @prayers = Prayers.all
   end
+
 end
---
+++ app/models/prayer.rb
@@ -0,0 +1,3 @@
+class Prayer < ApplicationRecord
+  self.prayers = "my_prayers"
+end
